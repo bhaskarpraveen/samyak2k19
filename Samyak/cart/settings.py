de88448cb8ac2b30df780b8b328358d5958013a9
bhaskarpraveen <praveennaidu264@gmail.com>
2020-03-04 11:32:40 +0530

    removed keys

--- Samyak/cart/settings.py
+++ Samyak/cart/settings.py
@@ -15,20 +15,20 @@ DEBUG=False
 
 ALLOWED_HOSTS = ['ec2-13-235-200-11.ap-south-1.compute.amazonaws.com','0.0.0.0','klusamyak.in','www.klusamyak.in','localhost','*']
 
-DEFAULT_FROM_EMAIL='technical.samyak2k19@gmail.com'
+DEFAULT_FROM_EMAIL='your-email'
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = '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'
+EMAIL_HOST_PASSWORD = "Your-password"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = False
 
 #API_KEY="test_d4044f09402d3410b710105e6dc"
 #AUTH_TOKEN="test_c25c6564a1630f1971b8bd10e7e"
 # Application definition
-API_KEY="4ccc5441d9da753781d931e2be3bbbdf"
-AUTH_TOKEN="3053d73950d684a1af77e22ccb29126e"
+API_KEY='YOUR-API-KEY'
+AUTH_TOKEN='YOUR-API-TOKEN'
 ######################
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -149,16 +149,16 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Stripe and Braintree Settings
 if DEBUG:
     # test keys
-    STRIPE_PUBLISHABLE_KEY =  'pk_test_dImhiL1GuFRFTxgUIJHaTxpN00PGAoxXbR'
-    STRIPE_SECRET_KEY = 'sk_test_fjJK4ZbAhQPYgiBeSQcxb7bf0058kFVL3L'
+    STRIPE_PUBLISHABLE_KEY =  'YOUR-KEY'
+    STRIPE_SECRET_KEY = 'YOUR-KEY'
     BT_ENVIRONMENT='sandbox'
-    BT_MERCHANT_ID='kbdt3n88f6zvpc2r'
-    BT_PUBLIC_KEY='59v4jp8rtvyb2cp5'
-    BT_PRIVATE_KEY='0c43511bbbb497afa54e209a5e23fd01'
+    BT_MERCHANT_ID='YOUR-ID'
+    BT_PUBLIC_KEY='YOUR-KEY'
+    BT_PRIVATE_KEY='YOUR-KEY'
 else:
     # live keys
-    STRIPE_PUBLISHABLE_KEY = 'pk_test_dImhiL1GuFRFTxgUIJHaTxpN00PGAoxXbR'
-    STRIPE_SECRET_KEY = 'sk_test_fjJK4ZbAhQPYgiBeSQcxb7bf0058kFVL3L'
+    STRIPE_PUBLISHABLE_KEY =  'YOUR-KEY'
+    STRIPE_SECRET_KEY = 'YOUR-KEY'
 
 
 
@@ -175,16 +175,15 @@ LOGIN_URL = '/profiles/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 
 
-PAYTM_MERCHANT_KEY = "5wXLBDC_Y9FXUM8K"
-PAYTM_MERCHANT_ID = "FfcDUH84619985301191"
+PAYTM_MERCHANT_KEY = 'YOUR-KEY'
+PAYTM_MERCHANT_ID = 'YOUR-KEY'
 HOST_URL = "https://klusamyak.in"
 PAYTM_CALLBACK_URL = "/paytm/response/"
 
 
-PAYTM_MERCHANT_KEY = "5wXLBDC_Y9FXUM8K"
-PAYTM_MERCHANT_ID = "FfcDUH84619985301191"
+
 PAYTM_WEBSITE = 'WEBSTAGING'
-HOST_URL = "https://klusamyak.in"
+
 '''
 In sandbox enviornment you can use following wallet credentials to login and make payment.
 Mobile Number : 7777777777
